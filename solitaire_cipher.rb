6500495f7a98c6f8d1ec79f12ba70b3c4fdd4a4b
ibotdotout <tkroputa@gmail.com>
2016-01-15 18:40:29 +0700

    Refacotr following Codacy Guide

--- solitaire_cipher.rb
+++ solitaire_cipher.rb
@@ -1,28 +1,26 @@
-#http://rubyquiz.com/quiz1.html
+# http://rubyquiz.com/quiz1.html
 
 class SolitaireCipher
-
   def initialize(keystream)
     @key = keystream
   end
 
   def sanitize(s)
-    #upcase && remove none any A-Z char
-    s = s.upcase.gsub(/([^A-z])/, "")
+    s = s.upcase
+    s = s.gsub(/([^A-z])/, "")
+  end
+
+  def separate_five_groups(s)
     s = s + "X" * ((5 - s.size % 5) % 5)
     out = ""
     (s.size / 5).times {|i| out << s[i*5, 5] << " "}
-    return out.strip
+    out.strip
   end
 
   def text_to_numbers(s)
-    out = Array.new
-    s.each_byte {|c|
-      if c >= "A".ord and c <= "Z".ord
-        out << c - "A".ord + 1
-      end
-    }
-    return out
+    out = []
+    s.each_byte {|c| out << c - "A".ord + 1 if c >= "A".ord && c <= "Z".ord }
+    out
   end
 
 end
--- tests/solitaire_cipher_test.rb
+++ tests/solitaire_cipher_test.rb
@@ -4,7 +4,6 @@ require "test/unit"
 require_relative "../solitaire_cipher"
 
 class SolitaireCipherTest < Test::Unit::TestCase
-
   def setup
     text = "Code in Ruby, live longer!"
     @cipher = SolitaireCipher.new(text)
@@ -12,14 +11,21 @@ class SolitaireCipherTest < Test::Unit::TestCase
 
   def test_santitze
     text = "Code in Ruby, live longer!"
-    expected = "CODEI NRUBY LIVEL ONGER"
+    expected = "CODEINRUBYLIVELONGER"
     result = @cipher.sanitize(text)
+    assert_equal result, expected
+  end
+
+  def test_separate_five_groups
+    text = "CODEINRUBYLIVELONGER"
+    expected= "CODEI NRUBY LIVEL ONGER"
+    result = @cipher.separate_five_groups(text)
     assert_equal result, expected
   end
 
   def test_text_to_number
     text = "CODEI NRUBY LIVEL ONGER"
-    expected = [3, 15, 4, 5, 9,  14, 18, 21, 2, 25,  12, 9, 22, 5, 12,  15, 14, 7, 5, 18]
+    expected = [3,15,4,5,9,14,18,21,2,25,12,9,22,5,12,15,14,7,5,18]
     result = @cipher.text_to_numbers(text)
     assert_equal result, expected
   end
